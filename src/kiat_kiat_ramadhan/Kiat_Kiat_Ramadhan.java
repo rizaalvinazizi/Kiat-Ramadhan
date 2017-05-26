3e8d04e0591dce4989efd76cdb6494094dc8a270
rizaalvin <rizaalvinazizi@gmail.com>
2017-05-27 05:37:20 +0700

    Kiat-Kiat Ramadhan

---
+++ src/kiat_kiat_ramadhan/Kiat_Kiat_Ramadhan.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kiat_kiat_ramadhan;
+
+/**
+ *
+ * @author rizaalvin
+ */
+public class Kiat_Kiat_Ramadhan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
